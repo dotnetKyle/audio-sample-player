ddf61bfcf86a9f214833f890e7acb5ae6b2560f8
Kyle <dotnetKyle@gmail.com>
2021-04-30 11:29:51 -0400

    initial commit

---
+++ src/AudioPlayerConsole/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using AudioSamplePlayer.Services;
+using NAudio.Wave;
+
+namespace AudioPlayerConsole
+{
+    class Program
+    {
+        static float currentVolume = 1.0f;
+        static PlaybackState currentPlaybackState = PlaybackState.Stopped;
+        static AudioSamplePlayerService audioSamplePlayer;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var myMusicDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+
+            var files = Directory.GetFiles(myMusicDirectory, "*.mp3");
+
+            for (int i = 0; i < files.Length; i++)
+                Console.WriteLine($"{i + 1} - {files[i]}");
+
+            string filePath = null;
+            while(filePath == null)
+            {
+                Console.WriteLine("\r\nChoose a file number.");
+                var number = Console.ReadLine();
+                int intNumb = -1;
+                if(int.TryParse(number, out intNumb))
+                {
+                    if(intNumb > 0 && intNumb < files.Length + 2)
+                        filePath = files[intNumb - 1];
+                }
+            }
+
+            audioSamplePlayer = new AudioSamplePlayerService(filePath, currentVolume);
+            
+            Console.WriteLine("Audio File Loaded..");
+
+            audioSamplePlayer.Play(currentPlaybackState, currentVolume);
+                
+            currentPlaybackState = PlaybackState.Playing;
+
+            Console.WriteLine("Current State: Playing");
+            Console.Write("Enter a command:");
+
+            Task.Run(updateUI);
+
+            while (true)
+            {
+                var line = Console.ReadLine().ToLower();
+
+                if (line == "pause")
+                {
+                    audioSamplePlayer.Pause();
+                    currentPlaybackState = PlaybackState.Paused;
+                }
+                if (line == "stop")
+                {
+                    audioSamplePlayer.Stop();
+                    currentPlaybackState = PlaybackState.Stopped;
+                }
+                if (line == "play")
+                {
+                    audioSamplePlayer.Play(currentPlaybackState, currentVolume);
+                    currentPlaybackState = PlaybackState.Playing;
+                }
+
+                if (line == "exit")
+                    break;
+            }
+
+            audioSamplePlayer.Dispose();
+        }
+
+        static string currentUiStatus = null;
+        static void updateUI()
+        {
+            while(true)
+            {
+                System.Threading.Thread.Sleep(33);
+
+                string newUiStatus = "";
+                if(currentPlaybackState == PlaybackState.Playing)
+                {
+                    var position = TimeSpan.FromSeconds(audioSamplePlayer.GetPositionInSeconds());
+                    var length = TimeSpan.FromSeconds(audioSamplePlayer.GetLengthInSeconds());
+
+                    newUiStatus = $"Playing:{position.Minutes}:{position:ss}/{length.Minutes}:{length:ss}";
+                }
+                else if (currentPlaybackState == PlaybackState.Paused)
+                {
+                    var position = TimeSpan.FromSeconds(audioSamplePlayer.GetPositionInSeconds());
+                    var length = TimeSpan.FromSeconds(audioSamplePlayer.GetLengthInSeconds());
+
+                    newUiStatus = $"Paused:{position.Minutes}:{position:ss}/{length.Minutes}:{length:ss}";
+                }
+                else if (currentPlaybackState == PlaybackState.Stopped)
+                {
+                    newUiStatus = $"Stopped";
+                }
+
+                if (currentUiStatus != newUiStatus)
+                {
+                    var currentPosition = Console.GetCursorPosition();
+
+                    Console.CursorVisible = false;
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+                    Console.Write(new string(' ', Console.BufferWidth));
+                    Console.SetCursorPosition(0, Console.CursorTop - 1);
+
+                    Console.WriteLine($"Current State: {newUiStatus}");
+
+                    Console.SetCursorPosition(currentPosition.Left, currentPosition.Top);
+                    Console.CursorVisible = true;
+
+                    currentUiStatus = newUiStatus;
+                }
+            }
+        }
+    }
+}
---
+++ src/AudioSamplePlayer/Services/AudioSamplePlayerService.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using NAudio;
+using NAudio.Wave;
+
+namespace AudioSamplePlayer.Services
+{
+    public class AudioSamplePlayerService : IDisposable
+    {
+        AudioFileReader _audioFileReader;
+        DirectSoundOut _output;
+        string _filePath;
+        float _currentVolume;
+
+        public AudioSamplePlayerService(string filepath, float volume)
+        {
+            _currentVolume = volume;
+            _filePath = filepath;
+
+            PlaybackStopType = PlaybackStoppedTypes.EndOfFile;
+        }
+
+        public PlaybackStoppedTypes PlaybackStopType { get; set; }
+
+        public enum PlaybackStoppedTypes
+        {
+            PlaybackStoppedByUser,
+            EndOfFile
+        }
+
+        private void initializeIfNeeded()
+        {
+            if (_audioFileReader == null)
+                initializeStream();
+            if (_output == null)
+                initializeOutput();
+        }
+        private void initializeStream()
+        {
+            if(_audioFileReader != null)
+            {
+                _audioFileReader.Dispose();
+                _audioFileReader = null;
+            }
+
+            _audioFileReader = new AudioFileReader(_filePath) { Volume = _currentVolume };
+        }
+        private void initializeOutput()
+        {
+            if (_output != null)
+            {
+                _output.Dispose();
+                _output = null;
+            }
+
+            _output = new DirectSoundOut(200);
+            _output.PlaybackStopped += _output_PlaybackStopped;
+            _output.Init(new WaveChannel32(_audioFileReader) { PadWithZeroes = false });
+        }
+
+        public void Play(PlaybackState playbackState, double currentVolumeLevel)
+        {
+            if (playbackState == PlaybackState.Stopped || playbackState == PlaybackState.Paused)
+            {
+                initializeIfNeeded();
+
+                _output.Play();
+            }
+
+            _audioFileReader.Volume = (float)currentVolumeLevel;
+
+            if (PlaybackResumed != null)
+            {
+                PlaybackResumed();
+            }
+        }
+
+        public void Stop()
+        {
+            if (_output != null)
+            {
+                _output.Stop();
+            }
+        }
+
+        public void Pause()
+        {
+            if (_output != null)
+            {
+                _output.Pause();
+
+                if (PlaybackPaused != null)
+                {
+                    PlaybackPaused();
+                }
+            }
+        }
+
+        public void TogglePlayPause(double currentVolumeLevel)
+        {
+            if (_output != null)
+            {
+                if (_output.PlaybackState == PlaybackState.Playing)
+                {
+                    Pause();
+                }
+                else
+                {
+                    Play(_output.PlaybackState, currentVolumeLevel);
+                }
+            }
+            else
+            {
+                Play(PlaybackState.Stopped, currentVolumeLevel);
+            }
+        }
+
+        public double GetLengthInSeconds()
+        {
+            if (_audioFileReader != null)
+            {
+                return _audioFileReader.TotalTime.TotalSeconds;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public double GetPositionInSeconds()
+        {
+            return _audioFileReader != null ? _audioFileReader.CurrentTime.TotalSeconds : 0;
+        }
+
+        public float GetVolume()
+        {
+            if (_audioFileReader != null)
+            {
+                return _audioFileReader.Volume;
+            }
+            return 1;
+        }
+
+        public void SetPosition(double value)
+        {
+            if (_audioFileReader != null)
+            {
+                _audioFileReader.CurrentTime = TimeSpan.FromSeconds(value);
+            }
+        }
+
+        public void SetVolume(float value)
+        {
+            if (_output != null)
+            {
+                _audioFileReader.Volume = value;
+            }
+        }
+
+        private void _output_PlaybackStopped(object sender, StoppedEventArgs e)
+        {
+            Dispose();
+            if (PlaybackStopped != null)
+            {
+                PlaybackStopped();
+            }
+        }
+
+        #region Disposable Implementation
+
+        private bool disposing;
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            // free all unmanaged resources 
+            if (_output != null)
+            {
+                if (_output.PlaybackState == PlaybackState.Playing)
+                {
+                    _output.Stop();
+                }
+                _output.Dispose();
+                _output = null;
+            }
+
+            if (_audioFileReader != null)
+            {
+                _audioFileReader.Dispose();
+                _audioFileReader = null;
+            }
+
+            if(disposing)
+            {
+                // free managed resources here
+            }
+        }
+        #endregion
+
+        public event Action PlaybackResumed; 
+        public event Action PlaybackStopped;
+        public event Action PlaybackPaused;
+
+        ~AudioSamplePlayerService()
+        {
+            Dispose(false);
+        }
+    }
+}
